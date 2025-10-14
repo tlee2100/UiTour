86774075bddda5577fed2a68485e3e33791d98ef
BMinh166 <23520962@gm.uit.edu.vn>
2025-10-14 10:02:45 +0700

    Add main Footer

--- Frontend/uitour-app/src/App.jsx
+++ Frontend/uitour-app/src/App.jsx
@@ -2,21 +2,34 @@ import { useState } from 'react'
 
 {/* Test header */}
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import './App.css';
 
 
 function App() {
   
 
   return (
-    <div>
+    <div className='app-layout'>
       {/* Gọi component Header */}
       <Header />
 
       {/* Nội dung khác */}
-      <main>
+      <main className="main-content">
         <h2>Trang Home</h2>
         <p>Nội dung thử nghiệm...</p>
+        <p>Nội dung mẫu</p>
+        <p>Nội dung mẫu</p>
+        <p>Nội dung mẫu</p>
+        <p>Nội dung mẫu</p>
+        <p>Nội dung mẫu</p>
+        <p>Nội dung mẫu</p>
+        <p>Nội dung mẫu</p>
       </main>
+
+      {/* Gọi component Footer */}
+      <Footer/>
+      {/* <Footer /> */}
     </div>
 
   );
