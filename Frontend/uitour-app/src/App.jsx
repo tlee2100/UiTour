3eb320d335975f6e2888aff7d26215a91a6a570d
BMinh166 <23520962@gm.uit.edu.vn>
2025-10-15 14:51:51 +0700

    Add InfoHeader

--- Frontend/uitour-app/src/App.jsx
+++ Frontend/uitour-app/src/App.jsx
@@ -2,9 +2,6 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import routes from './routes';
-{/* Test header */}
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import './App.css';
 
 
--- Frontend/uitour-app/src/pages/HomeInfoPage.jsx
+++ Frontend/uitour-app/src/pages/HomeInfoPage.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 import setdata from "../assets/mockdata/setdata"; // Corrected import
 import './HomeInfoPage.css';
 import Gallery from "./HomeInfo_component/Gallery";
+import InfoHeader from "./Info_components/InfoHeader";
 
 // Trang HomeInfoPage hiển thị thông tin chi tiết về một chỗ ở cụ thể
 export default function HomeInfoPage() {
   // Giả sử dữ liệu chỗ ở được lấy từ API hoặc props
   const images = setdata; // Corrected usage
 
-  return (
-    <Gallery images={images} /> // Updated to use images directly
+  return ( //null
+    <div className="home-info-page">
+      <InfoHeader /> {/* Updated to use InfoHeader */}
+      <Gallery images={images} /> {/* Updated to use images directly {/**/}
+    </div>
   );
 }
