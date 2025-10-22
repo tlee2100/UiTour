b64c762f1c8ccf02bf2994160948c76fa99e3dc9
BMinh166 <23520962@gm.uit.edu.vn>
2025-10-22 09:32:42 +0700

    WIP: Add ExperienceInfoPage

--- Frontend/uitour-app/src/routes/index.jsx
+++ Frontend/uitour-app/src/routes/index.jsx
@@ -2,13 +2,15 @@ import { Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import HomePage from "../pages/HomeInfoPage";
 import HomeInfoPage from "../pages/HomeInfoPage";
+import ExperienceInfoPage from "../pages/ExperienceInfoPage";
 
 const routes = (
   <Route path="/" element={<MainLayout />}>
 
     {/* Thay trang HomeInfoPage thành trang cần hiện trên web chính */}
     {/* Ví dụ: <Route index element={<HomePage />} /> */}
-    <Route index element={<HomeInfoPage />} />       {/* Hiện ở <Outlet /> */}
+    {/*------<Route index element={<ExperienceInfoPage />}---------*/}
+    <Route index element={<ExperienceInfoPage />} />       {/* Hiện ở <Outlet /> */}
     {/* Nếu test xong nhớ chỉnh lại để ko bị conflict */}
 
 
