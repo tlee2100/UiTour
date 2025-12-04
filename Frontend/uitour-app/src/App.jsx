199e2ea16555d033979b4f05e191118c854dab68
BMinh166 <23520962@gm.uit.edu.vn>
2025-12-04 14:20:09 +0700

    Add global loading layout

--- Frontend/uitour-app/src/App.jsx
+++ Frontend/uitour-app/src/App.jsx
@@ -6,27 +6,32 @@ import ErrorBoundary from './components/ErrorBoundary';
 import { CurrencyProvider } from './contexts/CurrencyContext';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { LanguageCurrencyModalProvider } from './contexts/LanguageCurrencyModalContext';
+import { GlobalLoadingProvider } from "./contexts/GlobalLoadingContext";
+import GlobalLoadingOverlay from "./components/GlobalLoadingOverlay";
 import AppRoutes from './routes/index';
 import './App.css';
 
 function App() {
   return (
     <ErrorBoundary>
-      <LanguageProvider>
-        <CurrencyProvider>
-          <LanguageCurrencyModalProvider>
-            <AppProvider>
-              <PropertyProvider>
-                <ExperienceProvider>
-                  <Router>
-                    <AppRoutes /> {/* Chỉnh trang hiện trên web ở file index.jsx của folder routes */}
-                  </Router>
-                </ExperienceProvider>
-              </PropertyProvider>
-            </AppProvider>
-          </LanguageCurrencyModalProvider>
-        </CurrencyProvider>
-      </LanguageProvider>
+      <GlobalLoadingProvider>
+        <LanguageProvider>
+          <CurrencyProvider>
+            <LanguageCurrencyModalProvider>
+              <AppProvider>
+                <PropertyProvider>
+                  <ExperienceProvider>
+                    <Router>
+                      <GlobalLoadingOverlay />
+                      <AppRoutes /> {/* Chỉnh trang hiện trên web ở file index.jsx của folder routes */}
+                    </Router>
+                  </ExperienceProvider>
+                </PropertyProvider>
+              </AppProvider>
+            </LanguageCurrencyModalProvider>
+          </CurrencyProvider>
+        </LanguageProvider>
+      </GlobalLoadingProvider>
     </ErrorBoundary>
   );
 }
